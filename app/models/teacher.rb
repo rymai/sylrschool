978c4831d7c14e0c2d8154eadebdf022dd26bbb3
Sylvere Coutable <sylvani@laposte.net>
2018-04-05 19:18:41 +0200

    export objects

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,7 +3,7 @@ class Teacher < ActiveRecord::Base
   before_save :set_custo
   validates_presence_of :name,:person_status, :firstname, :lastname, :email
   validates :name, uniqueness: true
-  belongs_to :matter
+  ########belongs_to :matter
   belongs_to :grade_context
   #####has_and_belongs_to_many :notebook, join_table: :notebook_teachers
   has_and_belongs_to_many :matter, join_table: :teacher_matters
